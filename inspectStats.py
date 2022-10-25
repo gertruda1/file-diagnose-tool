958aad22037d341711fda29b6898923481fa762c
panicball <57039046+panicball@users.noreply.github.com>
2022-10-25 20:20:23 +0300

    Update inspectStats.py

--- inspectStats.py
+++ inspectStats.py
@@ -1,9 +1,9 @@
 import os
-import json
 from os import listdir
 import os
 
-def inspectSummary(tempdir, newName):
+# funkcija summary failo perskaitymui - perskaito ir isaugo eilutes su konkreciomis reiksmemis jose
+def readSummaryFile(tempdir, newName):
     os.chdir(os.path.join(tempdir + '/' + newName))
 
     openRead = open('SUMMARY', 'r')
@@ -12,47 +12,56 @@ def inspectSummary(tempdir, newName):
 
     for line in openRead:
         for value in wantedValues:
-
             if value in line:
-                summaryFileContent.append(line)   
-    
-    dict = {}
+                summaryFileContent.append(line)     
+    return summaryFileContent
+
+
+# funkcija summary failo analizei - isrenka aktualia informacija pagal raktines reiksmes
+def summaryDictionary(tempdir, newName):
+    summaryFileContent = readSummaryFile(tempdir, newName)
+    summaryDict = {}
     
     size = len(summaryFileContent)
     for i in range(size):
-
         partitionedString = summaryFileContent[i].partition('=')
-
-        dict[partitionedString[0]] = partitionedString[2]
+        summaryDict[partitionedString[0]] = partitionedString[2]
         if partitionedString[0] == 'LOAD':
-            dict['CPU LOAD'] = dict.pop('LOAD')
+            summaryDict['CPU LOAD'] = summaryDict.pop('LOAD')
+    return summaryDict
 
-    for j in dict:
-        if j == 'RAM':
-            ramKB =  dict[j].partition(' ')
-        if j == 'CPU LOAD':
-            cpuLoad = dict[j].split(' ')
-    
+
+# funkcija failo turinio informacijai perskiatyti
+def getContent(tempdir, newName):
+    os.chdir(os.path.join(tempdir + '/' + newName))
     with open("SUMMARY", "r") as f:
         content = f.read()
-   
-    return dict, ramKB, cpuLoad, content
+    return content
 
 
+# funkcija ram ir cpu reiksmem grazinti
+def inspectSummary(tempdir, newName):
+    summaryDict = summaryDictionary(tempdir, newName)
+    
+    for j in summaryDict:
+        if j == 'RAM':
+            ramKB =  summaryDict[j].partition(' ')
+        if j == 'CPU LOAD':
+            cpuLoad = summaryDict[j].split(' ')
+    
+    return ramKB, cpuLoad
+
+
+# funkcija uzluzusiu procesu paieskai - isrenka procesus kurie atitinka bloga statusa indikuojancias reiksmes
 def inspectHangedProcesses(tempdir, newName):
-
     os.chdir(os.path.join(tempdir + '/' + newName + '/Raw_data_dumps'))
-
     if os.path.isfile('ps'):
         openRead = open('ps', 'r')
-
         statContent = [] 
-
         unwantedStates = ['D', 'X', 'T', 'Z', 't']
         commonStatCounter = 0
 
         for possition, line in enumerate(openRead):
-            
             if possition == 0:
                 stat = line.split('STAT')[0]
                 command = line.split('COMMAND')[0]
@@ -60,9 +69,7 @@ def inspectHangedProcesses(tempdir, newName):
                 
             if possition != 0:
                 partitionedState = line[len(stat):len(command)]
-                
                 partitionedState = partitionedState.replace(" ", "")
-
                 for i in unwantedStates:
                     if i in partitionedState:
                         
@@ -72,7 +79,6 @@ def inspectHangedProcesses(tempdir, newName):
         os.chdir(os.path.join(tempdir + '/' + newName))
 
         commonStatCounter += 1
-        statContent.append("  17 root         0 D   [watchdog/2]")
 
         if commonStatCounter == 0:
             statContentText='No hanged processes detected'
@@ -90,38 +96,30 @@ def inspectHangedProcesses(tempdir, newName):
         return statContent, statContentText 
 
 
+# funkcija pstore failo analizei - isrenka sistemos anomalijas pagal raktines reiksmes
 def pstore(tempdir, newName):
     
     os.chdir(os.path.join(tempdir + '/' + newName))
-
     searchValues = ['Oops:', 'oom-killer']
-    list_of_results = []
-    line_number = 0
+    listOfResults = []
+    lineNumber = 0
       
     if len(os.listdir(tempdir + '/' + newName + '/Raw_data_dumps/pstore') ) == 0: 
-        pstore_text = "Pstore folder is empty"
+        pstoreText = "Pstore folder is empty"
 
-    else:    
-                       
+    else:       
         for filename in listdir(tempdir + '/' + newName + '/Raw_data_dumps/pstore'): 
-                 
             with open(tempdir + '/' + newName + '/Raw_data_dumps/pstore' + '/' + filename, 'r') as read_obj: 
-
-                line_number = 0
-
+                lineNumber = 0
                 for line in read_obj:
-
-                    line_number += 1 
-
+                    lineNumber += 1 
                     for i in searchValues: 
-
                         if i in line:
-                            list_of_results.append((line_number, line.rstrip()))
+                            listOfResults.append((lineNumber, line.rstrip()))
                             
-                list_of_results.append("labas")
-                if list_of_results:
-                    pstore_text = 'Anomality detected in Pstore file: ' + filename 
+                if listOfResults:
+                    pstoreText = 'Anomality detected in Pstore file: ' + filename 
                 else:
-                    pstore_text="No craches or OOMs detected"
+                    pstoreText="No craches or OOMs detected"
 
-    return pstore_text, list_of_results
+    return pstoreText, listOfResults
